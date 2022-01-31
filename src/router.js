d1733cd14b189ecbd72d78ecf1b91f1558814a07
Leticia <leticiancalves1@gmail.com>
2022-01-31 00:06:09 -0300

    first commit

---
+++ src/router.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import Contato from "./views/Contato.vue";
+import Cursos from "./views/Cursos.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home
+    },
+    {
+      path: "/contato",
+      name: "Contato",
+      component: Contato
+    },
+    {
+      path: "/cursos",
+      name: "Cursos",
+      component: Cursos
+    }
+  ]
+});
---
+++ src/views/Contato.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Contato</h1>
+</template>
+
+<script>
+export default {
+  name: "contato"
+}
+
+</script>
